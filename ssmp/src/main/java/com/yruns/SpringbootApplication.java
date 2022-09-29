42cebf5531f5513b6cf19b11f755cb7984af12f7
ysh <2994056734@qq.com>
2022-09-29 17:42:06 +0800

    update

--- ssmp/src/main/java/com/yruns/SpringbootApplication.java
+++ ssmp/src/main/java/com/yruns/SpringbootApplication.java
@@ -1,11 +1,13 @@
 package com.yruns;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching      // 开启缓存
+//@EnableCaching      // 开启缓存
+@EnableCreateCacheAnnotation
 public class SpringbootApplication {
 
     public static void main(String[] args) {
--- ssmp/src/main/java/com/yruns/service/impl/MsgServiceImpl.java
+++ ssmp/src/main/java/com/yruns/service/impl/MsgServiceImpl.java
@@ -1,5 +1,8 @@
 package com.yruns.service.impl;
 
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.anno.CacheType;
+import com.alicp.jetcache.anno.CreateCache;
 import com.yruns.pojo.SMSCode;
 import com.yruns.service.MsgService;
 import com.yruns.utils.CodeUtils;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * MsgServiceImpl
@@ -17,16 +22,20 @@ public class MsgServiceImpl implements MsgService {
     @Autowired
     private CodeUtils codeUtils;
 
+    @CreateCache(area="default", name="jetCache", expire=60, timeUnit=TimeUnit.SECONDS, cacheType = CacheType.LOCAL)
+    private Cache<String, String> cache;
+
     @Override
-    @CachePut(value="smsCode", key="#tele")
     public String sendCodeToSMS(String tele) {
-        return codeUtils.generator(tele);
+        String code = codeUtils.generator(tele);
+        cache.put(tele, code);
+        return code;
     }
 
     @Override
     public boolean checkCode(SMSCode smsCode) {
         String code = smsCode.getCode();    // 拿出用户填写的code
-        String cacheCode = codeUtils.get(smsCode.getTele());   // 从内存中拿出真正的code
+        String cacheCode = cache.get(smsCode.getTele());   // 从内存中拿出真正的code
         return code.equals(cacheCode);
     }
 }
